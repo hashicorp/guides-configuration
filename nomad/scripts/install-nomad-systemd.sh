a13ea1008a83d39bd8fe9c91207e0bdb3a25a107
Dan Brown <roooms@gmail.com>
2017-04-20 20:08:40 +0100

    Apply changes made to consul to nomad
    This commit takes everything worked out on the consul install and applies to nomad.
    
    - nomad installs consul as well, but we should decide if consul would typically run as client or server on any nomad node
    - nomad has support for client and server modes but the configuration files are not yet automatically configured

--- nomad/scripts/install-nomad-systemd.sh
+++ nomad/scripts/install-nomad-systemd.sh
@@ -3,30 +3,43 @@ set -x
 
 logger() {
   DT=$(date '+%Y/%m/%d %H:%M:%S')
-  echo "$DT install-nomad-systemd.sh: $1"
+  FILENAME="install-consul-systemd.sh"
+  echo "$DT $FILENAME: $1"
 }
 
-logger "Installing Nomad systemd services"
-sudo cp /tmp/nomad/init/systemd/nomad.service /etc/systemd/system/nomad.service
-sudo cp /tmp/consul/init/systemd/consul.service /etc/systemd/system/consul.service
-sudo cp /tmp/consul/init/systemd/consul-online.service /etc/systemd/system/consul-online.service
-sudo cp /tmp/consul/init/systemd/consul-online.target /etc/systemd/system/consul-online.target
-sudo cp /tmp/consul/init/systemd/consul-online.sh /usr/bin/consul-online.sh
+logger "Running"
 
 # Detect package management system.
 YUM=$(which yum 2>/dev/null)
 APT_GET=$(which apt-get 2>/dev/null)
 
 if [[ ! -z ${YUM} ]]; then
+  SYSTEMD_DIR="/etc/systemd/system"
   logger "Installing systemd services for RHEL/CentOS"
-  sudo chmod 664 /etc/systemd/system/{nomad.*,consul.*}
+  sudo cp /tmp/nomad/init/systemd/nomad.service ${SYSTEMD_DIR}
+  sudo cp /tmp/consul/init/systemd/consul-online.service ${SYSTEMD_DIR}
+  sudo cp /tmp/consul/init/systemd/consul-online.target ${SYSTEMD_DIR}
+  sudo cp /tmp/consul/init/systemd/consul-online.sh /usr/bin/consul-online.sh
+  sudo chmod 0664 ${SYSTEMD_DIR}/{nomad*,consul*}
 elif [[ ! -z ${APT_GET} ]]; then
-  logger "Installing systemd services for Ubuntu/Debian"
-  sudo chmod 664 /lib/systemd/system/nomad*
-  sudo chmod 664 /lib/systemd/system/consul*
+  SYSTEMD_DIR="/lib/systemd/system"
+  logger "Installing systemd services for Debian/Ubuntu"
+  sudo cp /tmp/nomad/init/systemd/nomad.service ${SYSTEMD_DIR}
+  sudo cp /tmp/consul/init/systemd/consul-online.service ${SYSTEMD_DIR}
+  sudo cp /tmp/consul/init/systemd/consul-online.target ${SYSTEMD_DIR}
+  sudo cp /tmp/consul/init/systemd/consul-online.sh /usr/bin/consul-online.sh
+  sudo chmod 0664 ${SYSTEMD_DIR}/{nomad*,consul*}
 else
-  logger "OS Detection failed, ${USER} user not created."
+  logger "Service not installed due to OS detection failure"
   exit 1;
 fi
+
+#logger "Enabling and starting consul"
+#sudo systemctl enable consul
+#sudo systemctl start consul
+
+#logger "Enabling and starting nomad"
+#sudo systemctl enable nomad
+#sudo systemctl start nomad
 
 logger "Complete"
--- nomad/scripts/install-nomad.sh
+++ nomad/scripts/install-nomad.sh
@@ -3,54 +3,50 @@ set -x
 
 logger() {
   DT=$(date '+%Y/%m/%d %H:%M:%S')
-  echo "$DT nomad-install.sh: $1"
+  FILENAME="install-nomad.sh"
+  echo "$DT $FILENAME: $1"
 }
 
-NOMAD_VERSION=${VERSION:-0.5.6}
-NOMAD_URL=${1:-https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip}
-NOMAD_ZIP=/tmp/nomad_${NOMAD_VERSION}_linux_amd64.zip
-NOMAD_USER=${USER:-}
-NOMAD_GROUP=${GROUP:-}
-NOMAD_CLIENT=${CLIENT}
-NOMAD_SERVER=${SERVER}
-NOMAD_CONSUL=${CONSUL}
+logger "Running"
 
-cd /tmp
-logger "Downloading Nomad"
-curl -L -o $NOMAD_ZIP $NOMAD_URL
+NOMAD_VERSION=${VERSION:-"0.5.6"}
+NOMAD_ZIP="nomad_${NOMAD_VERSION}_linux_amd64.zip"
+NOMAD_URL=${URL:-"https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/${NOMAD_ZIP}"}
+NOMAD_CLIENT=${CLIENT:-}
+NOMAD_SERVER=${SERVER:-}
+NOMAD_CONSUL=${CONSUL:-}
+NOMAD_USER=${USER:-"nomad"}
+NOMAD_GROUP=${GROUP:-"nomad"}
+CONFIG_DIR="/etc/nomad.d"
+DATA_DIR="/opt/nomad/data"
+DOWNLOAD_DIR="/tmp"
 
-logger "Unpacking Nomad"
-unzip -p $NOMAD_ZIP > nomad.bin
-rm -f $NOMAD_ZIP
+logger "Downloading nomad ${NOMAD_VERSION}"
+curl --silent --output ${DOWNLOAD_DIR}/${NOMAD_ZIP} ${NOMAD_URL}
 
-logger "Installing Nomad"
-sudo chmod +x nomad.bin
-sudo mv nomad.bin /usr/local/bin/nomad
+logger "Installing nomad"
+sudo unzip -o ${DOWNLOAD_DIR}/${NOMAD_ZIP} -d /usr/local/bin/
 sudo chmod 0755 /usr/local/bin/nomad
-sudo chown root:root /usr/local/bin/nomad
+sudo chown ${NOMAD_USER}:${NOMAD_GROUP} /usr/local/bin/nomad
 
-logger "Installed Nomad version is $(/usr/local/bin/nomad --version)"
+logger "/usr/local/bin/nomad --version: $(/usr/local/bin/nomad --version)"
 
-logger "Setup Nomad configuration and data directories"
-sudo mkdir -pm 0600 /etc/nomad.d /opt/nomad/data
+logger "Configuring nomad"
+sudo mkdir -pm 0755 ${CONFIG_DIR} ${DATA_DIR}
 sudo cp /tmp/nomad/config/nomad-default.hcl /etc/nomad.d/nomad-default.hcl
-sudo cp /tmp/nomad/config/nomad-consul.hcl /etc/nomad.d/nomad-consul.hcl
-sudo chmod -R 0755 /etc/nomad.d
-sudo chown -R ${NOMAD_USER}.${NOMAD_GROUP} /opt/nomad /etc/nomad.d
-
 if [[ ! -z ${NOMAD_CLIENT} ]]; then
-  logger "Setup Nomad client configuration"
+  logger "Configuring nomad as a client"
   sudo cp /tmp/nomad/config/nomad-client.hcl /etc/nomad.d/nomad-client.hcl
 fi
-
 if [[ ! -z ${NOMAD_SERVER} ]]; then
-  logger "Setup Nomad server configuration"
+  logger "Configuring nomad as a server"
   sudo cp /tmp/nomad/config/nomad-server.hcl /etc/nomad.d/nomad-server.hcl
 fi
-
 if [[ ! -z ${NOMAD_CONSUL} ]]; then
-  logger "Setup Nomad Consul configuration"
+  logger "Configuring nomad/consul"
   sudo cp /tmp/nomad/config/nomad-consul.hcl /etc/nomad.d/nomad-consul.hcl
 fi
+sudo chown -R ${NOMAD_USER}:${NOMAD_GROUP} ${CONFIG_DIR} ${DATA_DIR}
+sudo chmod -R 0644 ${CONFIG_DIR}/*
 
 logger "Complete"
