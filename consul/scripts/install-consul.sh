af315d0b6eb71bc6fcdce2c3c0a50f97f3be1ffa
Dan Brown <roooms@gmail.com>
2017-05-30 16:37:47 +0100

    Refactor vagrant and packer builds
    - update packer to copy all files to instance to reduce provisioner steps
    - update vagrant to copy all files to instance to reduce provisioner steps
    - run scripts inline (now they are all local) so 'logger' can report script activity better
    - remove attempts to configure at build and instead copy all config ready for runtime setup (vagrant or terraform)
    - add .example extension to all config so it doesn't get loaded automatically and can be configured at runtime

--- consul/scripts/install-consul.sh
+++ consul/scripts/install-consul.sh
@@ -3,8 +3,7 @@ set -x
 
 logger() {
   DT=$(date '+%Y/%m/%d %H:%M:%S')
-  FILENAME="install-consul.sh"
-  echo "$DT $FILENAME: $1"
+  echo "$DT $0: $1"
 }
 
 logger "Running"
@@ -12,9 +11,8 @@ logger "Running"
 CONSUL_VERSION="${VERSION}"
 CONSUL_ZIP="consul_${CONSUL_VERSION}_linux_amd64.zip"
 CONSUL_URL=${URL:-"https://releases.hashicorp.com/consul/${CONSUL_VERSION}/${CONSUL_ZIP}"}
-CONSUL_SERVER=${SERVER:-}
-CONSUL_USER=${USER:-"consul"}
-CONSUL_GROUP=${GROUP:-"consul"}
+CONSUL_USER="consul"
+CONSUL_GROUP="consul"
 CONFIG_DIR="/etc/consul.d"
 DATA_DIR="/opt/consul/data"
 DOWNLOAD_DIR="/tmp"
@@ -26,20 +24,12 @@ logger "Installing consul"
 sudo unzip -o ${DOWNLOAD_DIR}/${CONSUL_ZIP} -d /usr/local/bin/
 sudo chmod 0755 /usr/local/bin/consul
 sudo chown ${CONSUL_USER}:${CONSUL_GROUP} /usr/local/bin/consul
-
-logger "/usr/local/bin/consul --version: $(/usr/local/bin/consul --version)"
-
-logger "Configuring consul"
 sudo mkdir -pm 0755 ${CONFIG_DIR} ${DATA_DIR}
-sudo cp /tmp/consul/config/consul-default.json ${CONFIG_DIR}
-
-if [[ "${CONSUL_SERVER}" = "true" ]]; then
-  logger "Configuring consul as a server"
-  sudo cp /tmp/consul/config/consul-server.json ${CONFIG_DIR}
-fi
-
+sudo cp /tmp/consul/config/*.example ${CONFIG_DIR}
 sudo chown -R ${CONSUL_USER}:${CONSUL_GROUP} ${CONFIG_DIR} ${DATA_DIR}
 sudo chmod -R 0644 ${CONFIG_DIR}/*
+
+logger "/usr/local/bin/consul --version: $(/usr/local/bin/consul --version)"
 
 # Detect package management system.
 YUM=$(which yum 2>/dev/null)
