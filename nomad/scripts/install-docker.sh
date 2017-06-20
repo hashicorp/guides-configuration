c51232f7a9e82b465a6f518f9bd4e34b504169cf
Nicolas Corrarello <nicolas@corrarello.com>
2017-06-20 19:12:56 +0100

    Adding Docker to Nomad image

---
+++ nomad/scripts/install-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+logger() {
+  DT=$(date '+%Y/%m/%d %H:%M:%S')
+  echo "$DT docker.sh: $1"
+}
+
+logger "Executing"
+
+USER=$1
+
+logger "Installing Docker"
+
+curl -sSL https://get.docker.com/ | sh
+sh -c "echo \"DOCKER_OPTS='--dns 127.0.0.1 --dns 8.8.8.8 --dns-search service.consul'\" >> /etc/default/docker"
+usermod -aG docker $USER
+
+service docker restart
+
+logger "Completed"
