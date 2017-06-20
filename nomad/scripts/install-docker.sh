d7c92235a2be42956168b2c01be0ea1a4ea84120
Nicolas Corrarello <nicolas@corrarello.com>
2017-06-20 19:21:49 +0100

    Fix in the Build. Added docker install to Hashi-stack

--- nomad/scripts/install-docker.sh
+++ nomad/scripts/install-docker.sh
@@ -12,9 +12,9 @@ USER=$1
 
 logger "Installing Docker"
 
-curl -sSL https://get.docker.com/ | sh
-sh -c "echo \"DOCKER_OPTS='--dns 127.0.0.1 --dns 8.8.8.8 --dns-search service.consul'\" >> /etc/default/docker"
-usermod -aG docker $USER
+curl -sSL https://get.docker.com/ | sudo sh
+sudo sh -c "echo \"DOCKER_OPTS='--dns 127.0.0.1 --dns 8.8.8.8 --dns-search service.consul'\" >> /etc/default/docker"
+sudo usermod -aG docker $USER
 
 service docker restart
 
