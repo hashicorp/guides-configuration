733687126dc3523383f99637b5e95509d5fef304
Nicolas Corrarello <nicolas@corrarello.com>
2017-06-21 09:21:20 +0100

    Removing usermod as it is not relevant to this case

--- nomad/scripts/install-docker.sh
+++ nomad/scripts/install-docker.sh
@@ -8,13 +8,11 @@ logger() {
 
 logger "Executing"
 
-USER=$1
 
 logger "Installing Docker"
 
 curl -sSL https://get.docker.com/ | sudo sh
 sudo sh -c "echo \"DOCKER_OPTS='--dns 127.0.0.1 --dns 8.8.8.8 --dns-search service.consul'\" >> /etc/default/docker"
-sudo usermod -aG docker $USER
 
 service docker restart
 
