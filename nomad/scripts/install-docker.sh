9463538fba9daab47438f697938bfb1139e3833d
Nicolas Corrarello <nicolas@corrarello.com>
2017-06-21 09:30:12 +0100

    Removing service restart

--- nomad/scripts/install-docker.sh
+++ nomad/scripts/install-docker.sh
@@ -14,6 +14,4 @@ logger "Installing Docker"
 curl -sSL https://get.docker.com/ | sudo sh
 sudo sh -c "echo \"DOCKER_OPTS='--dns 127.0.0.1 --dns 8.8.8.8 --dns-search service.consul'\" >> /etc/default/docker"
 
-service docker restart
-
 logger "Completed"
