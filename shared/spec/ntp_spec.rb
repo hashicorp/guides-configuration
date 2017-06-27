d7f375faa263dbabd49063192d39ec3374b72807
Nicolas Corrarello <nicolas@corrarello.com>
2017-06-27 21:46:10 +0100

    NTP service has a diferent name in Ubuntu

--- shared/spec/ntp_spec.rb
+++ shared/spec/ntp_spec.rb
@@ -4,7 +4,12 @@ describe package('ntp') do
   it { should be_installed }
 end
 
-describe service('ntpd') do
+describe service('ntpd'), :if => os[:family] == 'redhat' do
+  it { should be_enabled   }
+  it { should be_running   }
+end
+
+describe service('ntp'), :if => os[:family] == 'debian' do
   it { should be_enabled   }
   it { should be_running   }
 end
