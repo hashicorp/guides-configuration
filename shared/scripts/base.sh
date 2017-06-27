a8dd65dfc830af520d8719fe749bbf236021ec0e
Nicolas Corrarello <nicolas@corrarello.com>
2017-06-27 18:45:12 +0100

    Service is called ntpd

--- shared/scripts/base.sh
+++ shared/scripts/base.sh
@@ -24,8 +24,8 @@ if [[ ! -z ${YUM} ]]; then
   logger "Performing updates and installing prerequisites"
   sudo yum -y check-update
   sudo yum install -q -y wget unzip bind-utils ruby rubygems ntp
-  sudo systemctl start ntp.service
-  sudo systemctl enable ntp.service
+  sudo systemctl start ntpd.service
+  sudo systemctl enable ntpd.service
 elif [[ ! -z ${APT_GET} ]]; then
   logger "Debian/Ubuntu system detected"
   logger "Performing updates and installing prerequisites"
