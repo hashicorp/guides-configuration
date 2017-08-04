2424e01eb6dbb622e3fe5c47fdb1ad86221a0a21
Nicolas Corrarello <nicolas@corrarello.com>
2017-08-04 15:13:44 +0100

    Adding function to build enterprise binaries

--- ci-functions.sh
+++ ci-functions.sh
@@ -33,3 +33,22 @@ build () {
     cd -
   done
 }
+
+build_ent () {
+  echo "-----BEGIN PGP PRIVATE KEY BLOCK-----\n${PGP_SECRET_KEY}\n-----END PGP PRIVATE KEY BLOCK-----" | gpg --import
+  AWS_SECRET_KEY_ID=$(echo $AWS_SECRET_PGP | base64 -D | gpg -d -)
+  CONSUL_URL=$(awscli presign s3://${S3BUCKET}/consul-enterprise/consul-enterprise_${CONSUL_VERSION}+ent_linux_amd64.zip --expires-in 60)
+  VAULT_URL=$(awscli presign s3://${S3BUCKET}/vault-enterprise/vault-enterprise_${VAULT_VERSION}+ent_linux_amd64.zip --expires-in 60)
+  for PRODUCT in $*; do
+    echo "Building ${PRODUCT} template ...             "
+    cd "${BUILDDIR}/${PRODUCT}"
+    if /tmp/packer build *.json ; then
+      echo -e "\033[32m${PRODUCT} \033[1m[PASS]\033[0m"
+    else
+      echo -e "\033[31m${PRODUCT} \033[1m[FAIL]\033[0m"
+      return 1
+    fi
+    cd -
+  done
+  gpg --delete-secret-keys ${PGP_SECRET_ID}  
+}
