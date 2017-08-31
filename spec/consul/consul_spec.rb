6bb1766eae7a74cac2ee4d5ad5dd1beb0cda4f21
Jon Benson <bensojon@gmail.com>
2017-08-30 21:51:54 -0700

    Update tests

--- spec/consul/consul_spec.rb
+++ spec/consul/consul_spec.rb
@@ -10,26 +10,12 @@ describe service('consul') do
   it { should be_running }
 end
 
-describe file('/etc/consul.d') do
-  it { should be_directory }
-end
-
 describe user('consul') do
   it { should exist }
 end
 
-file('/etc/consul.d/consul-default.json') do
-  it { should exist }
-  its(:content_as_json) { should include('data_dir' => '/opt/consul/data') }
-  its(:content_as_json) { should include('ui' => 'true') }
-  it { should be_readable.by('consul') }
-end
-
-file('/etc/consul.d/consul-server.json') do
-  it { should exist }
-  its(:content_as_json) { should include('server' => 'true') }
-  its(:content_as_json) { should include('bootstrap_expect' => '1') }
-  it { should be_readable_by('consul') }
+describe file('/etc/consul.d') do
+  it { should be_directory }
 end
 
 file('/opt/consul/data') do
@@ -38,6 +24,24 @@ file('/opt/consul/data') do
   it { should be_readable_by('consul') }
 end
 
+file('/etc/consul.d/consul-default.json.example') do
+  it { should exist }
+  its(:content_as_json) { should include('advertise_addr' => '127.0.0.1') }
+  its(:content_as_json) { should include('data_dir' => '/opt/consul/data') }
+  its(:content_as_json) { should include('client_addr' => '0.0.0.0') }
+  its(:content_as_json) { should include('log_level' => 'INFO') }
+  its(:content_as_json) { should include('ui' => 'true') }
+  it { should be_readable.by('consul') }
+end
+
+file('/etc/consul.d/consul-server.json.example') do
+  it { should exist }
+  its(:content_as_json) { should include('server' => 'true') }
+  its(:content_as_json) { should include('bootstrap_expect' => '1') }
+  its(:content_as_json) { should include('leave_on_terminate' => 'true') }
+  it { should be_readable_by('consul') }
+end
+
 describe command('curl http://localhost:8500/v1/status/leader -sL -w "%{http_code}\\n" -o /dev/null') do
-  its(:stdout) { should match /200/ }
+  its(:stdout) { should match (/200/) }
 end
--- spec/nomad/nomad_spec.rb
+++ spec/nomad/nomad_spec.rb
@@ -6,39 +6,52 @@ describe file('/usr/local/bin/nomad') do
 end
 
 describe service('nomad') do
-  it { should be_enabled   }
-  it { should be_running   }
+  it { should be_enabled }
+  it { should be_running }
 end
 
 describe file('/etc/nomad.d') do
   it { should be_directory }
 end
 
-file('/etc/nomad.d/nomad-server.hcl') do
-  it { should exist }
-  its(:content) { should match /true/ }
-end
-
-file('/etc/nomad.d/nomad-client.hcl') do
-  it { should exist }
-  its(:content) { should match /true/ }
-end
-
-file('/etc/nomad.d/nomad-default.hcl') do
-  it { should exist }
-  its(:content) { should match /\/opt\/nomad\/data/ }
-end
-
-file('/etc/nomad.d/nomad-consul.hcl') do
-  it { should exist }
-  its(:content) { should match /127.0.0.1:8500/ }
-  its(:content) { should match /auto_advertise = true/ }
-  its(:content) { should match /client_auto_join    = true/ }
-  its(:content) { should match /server_auto_join    = true/ }
-end
-
 file('/opt/nomad/data') do
   it { should be_directory }
+end
+
+file('/etc/nomad.d/nomad-client.hcl.example') do
+  it { should exist }
+  its(:content) { should match (/client/) }
+  its(:content) { should match (/enabled         = true/) }
+  its(:content) { should match (/client_max_port = 15000/) }
+  its(:content) { should match (/options/) }
+  its(:content) { should match (/"docker.cleanup.image"   = "0"/) }
+  its(:content) { should match (/"driver.raw_exec.enable" = "1"/) }
+end
+
+file('/etc/nomad.d/nomad-consul.hcl.example') do
+  it { should exist }
+  its(:content) { should match (/consul/) }
+  its(:content) { should match (/address        = "127.0.0.1:8500"/) }
+  its(:content) { should match (/auto_advertise = true/) }
+  its(:content) { should match (/client_service_name = "nomad-client"/) }
+  its(:content) { should match (/client_auto_join    = true/) }
+  its(:content) { should match (/server_service_name = "nomad-server"/) }
+  its(:content) { should match (/server_auto_join    = true/) }
+end
+
+file('/etc/nomad.d/nomad-default.hcl.example') do
+  it { should exist }
+  its(:content) { should match (/data_dir     = "\/opt\/nomad\/data"/) }
+  its(:content) { should match (/log_level    = "INFO"/) }
+  its(:content) { should match (/enable_debug = true/) }
+end
+
+file('/etc/nomad.d/nomad-server.hcl.example') do
+  it { should exist }
+  its(:content) { should match (/server/) }
+  its(:content) { should match (/enabled          = true/) }
+  its(:content) { should match (/bootstrap_expect = 1/) }
+  its(:content) { should match (/heartbeat_grace  = "30s"/) }
 end
 
 describe command('curl http://localhost:4646/v1/status/leader -sL -w "%{http_code}\\n" -o /dev/null'), :if => os[:family] == 'redhat' do
