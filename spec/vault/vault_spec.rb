0abee537c5db473479f9ebddc658ee58e54ab239
Jon Benson <bensojon@gmail.com>
2017-08-30 21:51:54 -0700

    Update tests

--- spec/vault/vault_spec.rb
+++ spec/vault/vault_spec.rb
@@ -11,7 +11,7 @@ describe file('/usr/local/bin/consul') do
 end
 
 describe command('/sbin/getcap /usr/local/bin/vault') do
-  its(:stdout) { should match /cap_ipc_lock\+ep/ }
+  its(:stdout) { should match (/cap_ipc_lock\+ep/) }
   its(:exit_status) { should eq 0 }
 end
 
@@ -20,28 +20,41 @@ describe service('vault') do
   it { should be_running }
 end
 
-describe file('/etc/vault.d') do
-  it { should be_directory }
-end
-
 describe user('vault') do
   it { should exist }
 end
 
-file('/etc/vault.d/vault-consul.hcl') do
+describe file('/etc/vault.d') do
+  it { should be_directory }
+end
+
+describe file('/etc/ssl/vault') do
+  it { should be_directory }
+end
+
+file('/etc/vault.d/vault-consul.hcl.example') do
   it { should exist }
-  its(:content) { should match /127.0.0.1:8500/ }
-  its(:content) { should match /path/ }
+  its(:content) { should match (/127.0.0.1:8500/) }
+  its(:content) { should match (/path/) }
   it { should be_readable.by('vault') }
 end
 
-file('/etc/vault.d/vault-no-tls.hcl') do
+file('/etc/vault.d/vault-no-tls.hcl.example') do
   it { should exist }
-  its(:content) { should match /tls_disable = 1/ }
-  its(:content) { should match /0.0.0.0:8200/ }
+  its(:content) { should match (/0.0.0.0:8200/) }
+  its(:content) { should match (/tls_disable = 1/) }
+  it { should be_readable_by('vault') }
+end
+
+file('/etc/vault.d/vault-tls.hcl.example') do
+  it { should exist }
+  its(:content) { should match (/0.0.0.0:8200/) }
+  its(:content) { should match (/tls_disable   = 0/) }
+  its(:content) { should match (/tls_cert_file/) }
+  its(:content) { should match (/tls_key_file/) }
   it { should be_readable_by('vault') }
 end
 
 describe command('curl http://localhost:8200/v1/sys/health -sL -w "%{http_code}\\n" -o /dev/null') do
-  its(:stdout) { should match /501/ }
+  its(:stdout) { should match (/200/) }
 end
