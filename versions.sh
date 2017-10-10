d3d85e60f62494a1d1d5fcae76eedd1543a7fa3f
Chad Armitstead <chad@hashicorp.com>
2017-10-10 08:45:46 +0100

    0.8.2.1 is not available on releases.hashicorp.com and that is breaking CI builds

--- versions.sh
+++ versions.sh
@@ -1,6 +1,6 @@
 export CONSUL_VERSION="0.9.3"
 export NOMAD_VERSION="0.6.3"
-export VAULT_VERSION="0.8.2.1"
+export VAULT_VERSION="0.8.3"
 export PACKER_VERSION="0.12.3"
 export TERRAFORM_VERSION="0.10.0"
 export RHEL_VERSION="7.3"
