c14b92d259c155a78436611f7caebe4fb310f21d
Jon Benson <bensojon@gmail.com>
2017-11-11 17:53:35 -0800

    Update download

--- shared/scripts/download-guides-configuration.sh
+++ shared/scripts/download-guides-configuration.sh
@@ -17,12 +17,10 @@ APT_GET=$(which apt-get 2>/dev/null)
 logger "Installing git"
 if [[ ! -z ${YUM} ]]; then
   logger "RHEL/CentOS system detected"
-  logger "Performing updates and installing prerequisites"
   sudo yum -y check-update
   sudo yum install -q -y git
 elif [[ ! -z ${APT_GET} ]]; then
   logger "Debian/Ubuntu system detected"
-  logger "Performing updates and installing prerequisites"
   sudo apt-get -qq -y update
   sudo apt-get install -qq -y git
 else
@@ -32,9 +30,10 @@ fi
 
 logger "Cloning guides-configuration repo"
 git clone https://github.com/hashicorp/guides-configuration.git
+cp -a guides-configuration/. .
+rm -rf guides-configuration
 
 # TODO: Remove when merged
-cd guides-configuration
 git checkout f-refactor
 
 logger "Complete"
