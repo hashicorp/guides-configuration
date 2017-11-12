59d1b7cc33a0f31a949e816c0f67e1466ff6bcb9
Jon Benson <bensojon@gmail.com>
2017-11-11 17:37:39 -0800

    Install git

--- shared/scripts/download-guides-configuration.sh
+++ shared/scripts/download-guides-configuration.sh
@@ -10,6 +10,26 @@ logger "Running"
 
 cd /tmp
 
+# Detect package management system.
+YUM=$(which yum 2>/dev/null)
+APT_GET=$(which apt-get 2>/dev/null)
+
+logger "Installing git"
+if [[ ! -z ${YUM} ]]; then
+  logger "RHEL/CentOS system detected"
+  logger "Performing updates and installing prerequisites"
+  sudo yum -y check-update
+  sudo yum install -q -y git
+elif [[ ! -z ${APT_GET} ]]; then
+  logger "Debian/Ubuntu system detected"
+  logger "Performing updates and installing prerequisites"
+  sudo apt-get -qq -y update
+  sudo apt-get install -qq -y git
+else
+  logger "Prerequisites not installed due to OS detection failure"
+  exit 1;
+fi
+
 logger "Cloning guides-configuration repo"
 git clone https://github.com/hashicorp/guides-configuration.git
 
