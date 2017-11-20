hashicorp/guides-configuration-deprecated

Update CI, Packer templates, and Vagrantfiles

ci-functions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # This script includes a set of generic CI functions to test Packer Builds.
 prepare () {
   rm -rf /tmp/packer
@@ -12,17 +14,20 @@ prepare () {
 
 validate () {
   for PRODUCT in $*; do
-    echo "Reviewing ${PRODUCT} template ...             "
+    echo "Reviewing ${PRODUCT}.json template..."
     cd "${BUILDDIR}/${PRODUCT}"
+
     if /tmp/packer validate ${PRODUCT}.json; then
       echo -e "\033[32m\033[1m[PASS]\033[0m"
     else
       echo -e "\033[31m\033[1m[FAIL]\033[0m"
       return 1
     fi
+
     cd -
   done
-  echo "Reviewing shell scripts ..."
+
+  echo "Reviewing shell scripts..."
   if find . -iname \*.sh -exec bash -n {} \; > /dev/null; then
     echo -e "\033[32m\033[1m[PASS]\033[0m"
   else
@@ -31,63 +36,78 @@ validate () {
   fi
 }
 
-build () {
-  export CONSUL_RELEASE="${CONSUL_VERSION}"
-  export VAULT_RELEASE="${VAULT_VERSION}"
-  export DISTRIBUTION="oss"
+packer_build () {
+  echo ${PGP_SECRET_KEY} | base64 -d | gpg --import
+  echo "Building Consul version: ${CONSUL_VERSION}"
+  echo "Building Vault version: ${VAULT_VERSION}"
+  echo "Building Nomad version: ${NOMAD_VERSION}"
+
+  if [[ ${CONSUL_VERSION} == *"ent"* ]]; then
+    export CONSUL_VERSION_STRIPPED=${CONSUL_VERSION/"+ent"/}
+    export CONSUL_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/consul-enterprise/${CONSUL_VERSION_STRIPPED}/consul-enterprise_${CONSUL_VERSION}_linux_amd64.zip --expires-in 3600)
+  fi
+
+  if [[ ${VAULT_VERSION} == *"ent"* ]]; then
+    export VAULT_VERSION_STRIPPED=${VAULT_VERSION/"+ent"/}
+    export VAULT_DOWNLOAD_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/vault-enterprise/${VAULT_VERSION_STRIPPED}/vault-enterprise_${VAULT_VERSION_STRIPPED}_linux_amd64.zip --expires-in 3600)
+  fi
+
   for PRODUCT in $*; do
-    echo "Building ${PRODUCT} template ...             "
+    echo "Building ${PRODUCT}.json Packer template..."
     cd "${BUILDDIR}/${PRODUCT}"
-    export TMPDIR="/tmp/${PRODUCT}-$((1 + RANDOM % 100))"
-    mkdir -p $TMPDIR
+
     if /tmp/packer build ${PRODUCT}.json ; then
       echo -e "\033[32m${PRODUCT} \033[1m[PASS]\033[0m"
     else
       echo -e "\033[31m${PRODUCT} \033[1m[FAIL]\033[0m"
       return 1
     fi
+
     cd -
   done
+
+  echo "Cleaning up GPG Keyring..."
+  gpg --fingerprint --with-colons ${PGP_SECRET_ID} |\
+    grep "^fpr" |\
+    sed -n 's/^fpr:::::::::\([[:alnum:]]\+\):/\1/p' |\
+    xargs gpg --batch --delete-secret-keys
 }
 
+# TODO: Remove when merging into master
 build_ent () {
-  echo ${PGP_SECRET_KEY} | base64 -d | gpg --import
-  export CONSUL_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/consul-enterprise/${CONSUL_VERSION}/consul-enterprise_${CONSUL_VERSION}+ent_linux_amd64.zip --expires-in 3600)
-  export VAULT_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/vault-enterprise/${VAULT_VERSION}/vault-enterprise_${VAULT_VERSION}_linux_amd64.zip --expires-in 3600)
-  export CONSUL_RELEASE="${CONSUL_VERSION}"
-  export NOMAD_RELEASE="${NOMAD_VERSION}"
-  export VAULT_RELEASE="${VAULT_VERSION}"
-  export CONSUL_VERSION="${CONSUL_VERSION}+ent"
-  export NOMAD_VERSION="${NOMAD_RELEASE}+ent"
-  export VAULT_VERSION="${VAULT_VERSION}+ent"
-  export DISTRIBUTION="ent"
-  for PRODUCT in $*; do
-    echo "Building ${PRODUCT} template ...             "
-    cd "${BUILDDIR}/${PRODUCT}"
-    export TMPDIR="/tmp/${PRODUCT}-$((1 + RANDOM % 100))"
-    mkdir -p $TMPDIR
-    if /tmp/packer build ${PRODUCT}.json ; then
-      echo -e "\033[32m${PRODUCT} \033[1m[PASS]\033[0m"
+  echo "built_ent is DEPRECATED: Remove this function when merging to master"
+}
+
+build () {
+  echo "Starting build from ${GIT_BRANCH}"
+
+  if [ -z ${RELEASE_VERSION} ]; then
+    # Set RELEASE_VERSION to the current git branch if not specified so it's not empty
+    export RELEASE_VERSION = ${GIT_BRANCH}
+  fi
+
+  if [ -z ${USER_TRIGGER+x} ]; then
+    export VCS_NAME = ${USER_TRIGGER}
+  fi
+
+  if [[ ${GIT_BRANCH} == *"master"* ]]; then
+    echo "Building ${RELEASE_VERSION} image from ${GIT_BRANCH}"
+    packer_build consul vault nomad hashistack
+  else
+    echo "FORCE_BUILD: ${FORCE_BUILD}"
+
+    if ! [ -z ${FORCE_BUILD} ]; then
+      echo "Building ${RELEASE_VERSION} image from ${GIT_BRANCH}"
+      packer_build consul vault nomad hashistack
     else
-      echo -e "\033[31m${PRODUCT} \033[1m[FAIL]\033[0m"
-      return 1
+      echo "Skip building ${RELEASE_VERSION} image from ${GIT_BRANCH}"
     fi
-    cd -
-  done
-  echo "Cleaning up GPG Keyring ...                   "
-  #gpg --fingerprint --with-colons ${PGP_SECRET_ID} |\
-  #  grep "^fpr" |\
-  #  sed -n 's/^fpr:::::::::\([[:alnum:]]\+\):/\1/p' |\
-  #  xargs gpg --batch --delete-secret-keys
-  rm -rf ~/.gnupg
-  echo "Resetting vars for subsequent runs ...        "
-  export VAULT_VERSION="${VAULT_RELEASE}"
-  export CONSUL_VERSION="${CONSUL_RELEASE}"
-  export NOMAD_VERSION="${NOMAD_RELEASE}"
+  fi
+
+  echo "Completed build from ${GIT_BRANCH}"
 }
 
 publish () {
-
   git clone https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/hashicorp-modules/image-permission-aws
   cd image-permission-aws
   /tmp/terraform init

hashicorp/guides-configuration

ci-functions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # This script includes a set of generic CI functions to test Packer Builds.
 prepare () {
   rm -rf /tmp/packer
@@ -12,17 +14,20 @@ prepare () {
 
 validate () {
   for PRODUCT in $*; do
-    echo "Reviewing ${PRODUCT} template ...             "
+    echo "Reviewing ${PRODUCT}.json template..."
     cd "${BUILDDIR}/${PRODUCT}"
+
     if /tmp/packer validate ${PRODUCT}.json; then
       echo -e "\033[32m\033[1m[PASS]\033[0m"
     else
       echo -e "\033[31m\033[1m[FAIL]\033[0m"
       return 1
     fi
+
     cd -
   done
-  echo "Reviewing shell scripts ..."
+
+  echo "Reviewing shell scripts..."
   if find . -iname \*.sh -exec bash -n {} \; > /dev/null; then
     echo -e "\033[32m\033[1m[PASS]\033[0m"
   else
@@ -31,66 +36,105 @@ validate () {
   fi
 }
 
-build () {
-  export CONSUL_RELEASE="${CONSUL_VERSION}"
-  export VAULT_RELEASE="${VAULT_VERSION}"
-  export DISTRIBUTION="oss"
+packer_build () {
+  echo ${PGP_SECRET_KEY} | base64 -d | gpg --import
+  echo "Building Consul version: ${CONSUL_VERSION}"
+  echo "Building Vault version: ${VAULT_VERSION}"
+  echo "Building Nomad version: ${NOMAD_VERSION}"
+
+  if [[ ${CONSUL_VERSION} == *"ent"* ]]; then
+    export CONSUL_VERSION_STRIPPED=${CONSUL_VERSION/"+ent"/}
+    export CONSUL_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/consul-enterprise/${CONSUL_VERSION_STRIPPED}/consul-enterprise_${CONSUL_VERSION}_linux_amd64.zip)
+
+    # Replacing '+' with '-' as '+' is an invalid character for the AMI name and
+    # the version isn't used during the install when 'CONSUL_ENT_URL' is populated
+    export CONSUL_VERSION=${CONSUL_VERSION/'+'/'-'}
+  fi
+
+  if [[ ${VAULT_VERSION} == *"ent"* ]]; then
+    export VAULT_VERSION_STRIPPED=${VAULT_VERSION/"+ent"/}
+    export VAULT_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/vault-enterprise/${VAULT_VERSION_STRIPPED}/vault-enterprise_${VAULT_VERSION_STRIPPED}_linux_amd64.zip)
+
+    # Replacing '+' with '-' as '+' is an invalid character for the AMI name and
+    # the version isn't used during the install when 'VAULT_ENT_URL' is populated
+    export VAULT_VERSION=${VAULT_VERSION/'+'/'-'}
+  fi
+
+  if [[ ${NOMAD_VERSION} == *"ent"* ]]; then
+    export NOMAD_VERSION_STRIPPED=${NOMAD_VERSION/"+ent"/}
+    export NOMAD_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/vault-enterprise/${NOMAD_VERSION_STRIPPED}/vault-enterprise_${NOMAD_VERSION_STRIPPED}_linux_amd64.zip)
+
+    # Replacing '+' with '-' as '+' is an invalid character for the AMI name and
+    # the version isn't used during the install when 'NOMAD_ENT_URL' is populated
+    export NOMAD_VERSION=${NOMAD_VERSION/'+'/'-'}
+  fi
+
   for PRODUCT in $*; do
-    echo "Building ${PRODUCT} template ...             "
+    echo "Building ${PRODUCT}.json Packer template..."
     cd "${BUILDDIR}/${PRODUCT}"
-    export TMPDIR="/tmp/${PRODUCT}-$((1 + RANDOM % 100))"
-    mkdir -p $TMPDIR
+
     if /tmp/packer build ${PRODUCT}.json ; then
       echo -e "\033[32m${PRODUCT} \033[1m[PASS]\033[0m"
     else
       echo -e "\033[31m${PRODUCT} \033[1m[FAIL]\033[0m"
       return 1
     fi
+
     cd -
   done
+
+  echo "Cleaning up GPG Keyring..."
+  gpg --fingerprint --with-colons ${PGP_SECRET_ID} |\
+    grep "^fpr" |\
+    sed -n 's/^fpr:::::::::\([[:alnum:]]\+\):/\1/p' |\
+    xargs gpg --batch --delete-secret-keys
 }
 
+# TODO: Remove when merging into master
 build_ent () {
-  echo ${PGP_SECRET_KEY} | base64 -d | gpg --import
-  export CONSUL_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/consul-enterprise/${CONSUL_VERSION}/consul-enterprise_${CONSUL_VERSION}+ent_linux_amd64.zip)
-  sleep 10
-  export VAULT_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/vault-enterprise/${VAULT_VERSION}/vault-enterprise_${VAULT_VERSION}_linux_amd64.zip)
-  sleep 10
-  export NOMAD_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/nomad-enterprise/${NOMAD_VERSION}/nomad-enterprise_${NOMAD_VERSION}+ent_linux_amd64.zip)
-  sleep 10
-  export CONSUL_RELEASE="${CONSUL_VERSION}"
-  export NOMAD_RELEASE="${NOMAD_VERSION}"
-  export VAULT_RELEASE="${VAULT_VERSION}"
-  export CONSUL_VERSION="${CONSUL_VERSION}+ent"
-  export NOMAD_VERSION="${NOMAD_RELEASE}+ent"
-  export VAULT_VERSION="${VAULT_VERSION}+ent"
-  export DISTRIBUTION="ent"
-  for PRODUCT in $*; do
-    echo "Building ${PRODUCT} template ...             "
-    cd "${BUILDDIR}/${PRODUCT}"
-    export TMPDIR="/tmp/${PRODUCT}-$((1 + RANDOM % 100))"
-    mkdir -p $TMPDIR
-    if /tmp/packer build ${PRODUCT}.json ; then
-      echo -e "\033[32m${PRODUCT} \033[1m[PASS]\033[0m"
+  echo "build_ent is DEPRECATED: Remove this function when merging to master"
+}
+
+build () {
+  if [ -z ${RELEASE_VERSION} ]; then
+    # Set RELEASE_VERSION to the current git branch if not specified so it's not empty
+    export RELEASE_VERSION = ${GIT_BRANCH}
+  fi
+
+  if [ -z ${USER_TRIGGER+x} ]; then
+    export VCS_NAME = ${USER_TRIGGER}
+  fi
+
+  echo "Starting build from ${GIT_BRANCH}"
+  echo "RELEASE_VERSION: ${RELEASE_VERSION}"
+  echo "VCS_NAME: ${VCS_NAME}"
+
+  if [[ ${GIT_BRANCH} == *"master"* ]]; then
+    echo "Building ${RELEASE_VERSION} images from ${GIT_BRANCH}"
+    packer_build consul vault nomad hashistack
+  else
+    echo "RUN_BUILD: ${RUN_BUILD}"
+
+    if ! [ -z ${RUN_BUILD} ]; then
+      echo "Building ${RELEASE_VERSION} images from ${GIT_BRANCH}"
+      packer_build consul vault nomad hashistack
     else
-      echo -e "\033[31m${PRODUCT} \033[1m[FAIL]\033[0m"
-      return 1
+      echo "Skip building ${RELEASE_VERSION} images from ${GIT_BRANCH}"
     fi
-    cd -
-  done
-  echo "Cleaning up GPG Keyring ...                   "
-  #gpg --fingerprint --with-colons ${PGP_SECRET_ID} |\
-  #  grep "^fpr" |\
-  #  sed -n 's/^fpr:::::::::\([[:alnum:]]\+\):/\1/p' |\
-  #  xargs gpg --batch --delete-secret-keys
-  rm -rf ~/.gnupg
-  echo "Resetting vars for subsequent runs ...        "
-  export VAULT_VERSION="${VAULT_RELEASE}"
-  export CONSUL_VERSION="${CONSUL_RELEASE}"
-  export NOMAD_VERSION="${NOMAD_RELEASE}"
+  fi
+
+  echo "Completed build from ${GIT_BRANCH}"
 }
 
 publish () {
+  # Exit early if not on master branch or RUN_BUILD env var not passed
+  echo "GIT_BRANCH: ${GIT_BRANCH}"
+  echo "RUN_PUBLISH: ${RUN_PUBLISH}"
+
+  if ! [[ ${GIT_BRANCH} == *"master"* ]] && [ -z ${RUN_PUBLISH} ]; then
+    echo "Do not publish, exit early"
+    exit 0
+  fi
 
   git clone https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/hashicorp-modules/image-permission-aws
   cd image-permission-aws