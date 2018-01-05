07f545fa9a17804f0ed48819fcdfd4c6d9a312be
Jon Benson <bensojon@gmail.com>
2018-01-05 14:29:32 -0800

    Start Docker after install

--- nomad/scripts/install-docker.sh
+++ nomad/scripts/install-docker.sh
@@ -21,4 +21,7 @@ fi
 
 sudo sh -c "echo \"DOCKER_OPTS='--dns 127.0.0.1 --dns 8.8.8.8 --dns-search service.consul'\" >> /etc/default/docker"
 
+sudo systemctl enable docker
+sudo systemctl start docker
+
 echo "Complete"
