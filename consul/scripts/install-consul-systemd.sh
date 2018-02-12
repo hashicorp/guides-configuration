93b9207a268ea978d5d8b296eee7761919dca0cd
Jon Benson <bensojon@gmail.com>
2018-02-11 22:15:57 -0800

    Just download systemd config

--- consul/scripts/install-consul-systemd.sh
+++ consul/scripts/install-consul-systemd.sh
@@ -10,17 +10,17 @@ APT_GET=$(which apt-get 2>/dev/null)
 if [[ ! -z ${YUM} ]]; then
   SYSTEMD_DIR="/etc/systemd/system"
   echo "Installing consul systemd service for RHEL/CentOS"
-  sudo cp /tmp/consul/init/systemd/consul.service ${SYSTEMD_DIR}
-  sudo chmod 0664 ${SYSTEMD_DIR}/consul.service
 elif [[ ! -z ${APT_GET} ]]; then
   SYSTEMD_DIR="/lib/systemd/system"
   echo "Installing consul systemd service for Debian/Ubuntu"
-  sudo cp /tmp/consul/init/systemd/consul.service ${SYSTEMD_DIR}
-  sudo chmod 0664 ${SYSTEMD_DIR}/consul.service
 else
   echo "Service not installed due to OS detection failure"
   exit 1;
 fi
+
+# sudo cp /tmp/consul/init/systemd/consul.service ${SYSTEMD_DIR}
+curl -o ${SYSTEMD_DIR}/consul.service https://raw.githubusercontent.com/hashicorp/guides-configuration/master/consul/init/systemd/consul.service
+sudo chmod 0664 ${SYSTEMD_DIR}/consul.service
 
 sudo systemctl enable consul
 sudo systemctl start consul
