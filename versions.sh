4d48441f7e0d8175dee8804b141b9dc69880b64e
Nicolas Corrarello <nicolas@corrarello.com>
2018-03-05 20:28:11 +0000

    Bump Vault and Consul version

--- versions.sh
+++ versions.sh
@@ -1,6 +1,6 @@
-export CONSUL_VERSION="1.0.2"
+export CONSUL_VERSION="1.0.6"
 export NOMAD_VERSION="0.7.1"
-export VAULT_VERSION="0.9.1"
+export VAULT_VERSION="0.9.5"
 export PACKER_VERSION="1.1.3"
 export TERRAFORM_VERSION="0.10.8"
 export RHEL_VERSION="7.3"
