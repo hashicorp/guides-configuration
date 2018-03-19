9a121d55e69971c923b6a67a9ec3d8f2d6d94869
Jon Benson <bensojon@gmail.com>
2018-03-19 01:42:35 -0700

    Update ci-functions.sh with new CI logic

--- ci-functions.sh
+++ ci-functions.sh
@@ -16,9 +16,9 @@ validate () {
   for TEMPLATE in $*; do
     echo "cd into ${TEMPLATE} directory"
     cd ${BUILDDIR}/$(echo ${TEMPLATE} | sed 's/-.*//')
-    echo "Reviewing ${TEMPLATE}-aws.json template..."
+    echo "Reviewing ${TEMPLATE}.json template..."
 
-    if /tmp/packer validate ${TEMPLATE}-aws.json; then
+    if /tmp/packer validate ${TEMPLATE}.json; then
       echo -e "\033[32m\033[1m[PASS]\033[0m"
     else
       echo -e "\033[31m\033[1m[FAIL]\033[0m"
@@ -37,6 +37,54 @@ validate () {
   fi
 }
 
+gpg_import () {
+  echo ${PGP_SECRET_KEY} | base64 -d | gpg --import
+}
+
+presign_ent_url () {
+  if [ $# -eq 0 ]; then
+    echo -e "\033[31m\033[1m[FAIL - no variables provided]\033[0m"
+    return 1
+  fi
+
+  if [ $1 -eq 0 ]; then
+    echo -e "\033[31m\033[1m[FAIL - no product name provided]\033[0m"
+    return 1
+  fi
+
+  if [ $2 -eq 0 ]; then
+    echo -e "\033[31m\033[1m[FAIL - no product version provided]\033[0m"
+    return 1
+  fi
+
+  _AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -)
+  _AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY}
+  _REGION="us-east-1"
+  _PRODUCT=$1
+  _VERSION=$2
+  echo "_REGION: ${_REGION}"
+  echo "_PRODUCT: ${_PRODUCT}"
+  echo "_VERSION: ${_VERSION}"
+
+  if [ ${_PRODUCT} = "consul" ]; then
+    _S3_URL=s3://${S3BUCKET}/consul-enterprise/${_VERSION}/consul-enterprise_${_VERSION}+ent_linux_amd64.zip
+  elif [ ${_PRODUCT} = "vault" ]; then
+    _S3_URL=s3://${S3BUCKET}/vault/prem/${_VERSION}/vault-enterprise_${_VERSION}+prem_linux_amd64.zip
+  elif [ ${_PRODUCT} = "nomad" ]; then
+    _S3_URL=s3://${S3BUCKET}/nomad-enterprise/${_VERSION}/nomad-enterprise_${_VERSION}+ent_linux_amd64.zip
+  else
+    echo -e "\033[31m\033[1m[FAIL - invalid product selection for S3 enterprise URL]\033[0m"
+    return 1
+  fi
+
+  echo "_S3_URL: ${_S3_URL}"
+  echo "$(AWS_SECRET_ACCESS_KEY=${_AWS_SECRET_ACCESS_KEY} \
+    AWS_ACCESS_KEY_ID=${_AWS_ACCESS_KEY_ID} \
+    aws s3 presign \
+    --region=${_REGION} \
+    ${_S3_URL} )"
+}
+
 build () {
   if [ -z ${RELEASE_VERSION} ]; then
     # Set RELEASE_VERSION to the current git branch if not specified so it's not empty
@@ -50,45 +98,55 @@ build () {
   echo "Starting build from ${GIT_BRANCH}"
   echo "RELEASE_VERSION: ${RELEASE_VERSION}"
   echo "VCS_NAME: ${VCS_NAME}"
-
-  echo ${PGP_SECRET_KEY} | base64 -d | gpg --import
   echo "Building Consul version: ${CONSUL_VERSION}"
   echo "Building Vault version: ${VAULT_VERSION}"
   echo "Building Nomad version: ${NOMAD_VERSION}"
 
   if [[ ${CONSUL_VERSION} == *"ent"* ]]; then
     export CONSUL_VERSION_STRIPPED=${CONSUL_VERSION/"+ent"/}
-    export CONSUL_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/consul-enterprise/${CONSUL_VERSION_STRIPPED}/consul-enterprise_${CONSUL_VERSION}_linux_amd64.zip)
+    export CONSUL_ENT_URL=$(presign_ent_url consul ${CONSUL_VERSION_STRIPPED})
+    echo "CONSUL_VERSION_STRIPPED: ${CONSUL_VERSION_STRIPPED}"
+    echo "CONSUL_ENT_URL: ${CONSUL_ENT_URL}"
+    # export CONSUL_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/consul-enterprise/${CONSUL_VERSION_STRIPPED}/consul-enterprise_${CONSUL_VERSION}_linux_amd64.zip)
 
     # Replacing '+' with '-' as '+' is an invalid character for the AMI name and
     # the version isn't used during the install when 'CONSUL_ENT_URL' is populated
     export CONSUL_VERSION=${CONSUL_VERSION/'+'/'-'}
+    echo "CONSUL_VERSION: ${CONSUL_VERSION}"
   fi
 
   if [[ ${VAULT_VERSION} == *"ent"* ]]; then
     export VAULT_VERSION_STRIPPED=${VAULT_VERSION/"+ent"/}
-    export VAULT_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/vault-enterprise/${VAULT_VERSION_STRIPPED}/vault-enterprise_${VAULT_VERSION_STRIPPED}_linux_amd64.zip)
+    export VAULT_ENT_URL=$(presign_ent_url vault ${VAULT_VERSION_STRIPPED})
+    echo "VAULT_VERSION_STRIPPED: ${VAULT_VERSION_STRIPPED}"
+    echo "VAULT_ENT_URL: ${VAULT_ENT_URL}"
+    # export VAULT_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/vault-enterprise/${VAULT_VERSION_STRIPPED}/vault-enterprise_${VAULT_VERSION_STRIPPED}_linux_amd64.zip)
 
     # Replacing '+' with '-' as '+' is an invalid character for the AMI name and
     # the version isn't used during the install when 'VAULT_ENT_URL' is populated
     export VAULT_VERSION=${VAULT_VERSION/'+'/'-'}
+    echo "VAULT_VERSION: ${VAULT_VERSION}"
   fi
 
   if [[ ${NOMAD_VERSION} == *"ent"* ]]; then
     export NOMAD_VERSION_STRIPPED=${NOMAD_VERSION/"+ent"/}
-    export NOMAD_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/vault-enterprise/${NOMAD_VERSION_STRIPPED}/vault-enterprise_${NOMAD_VERSION_STRIPPED}_linux_amd64.zip)
+    export NOMAD_ENT_URL=$(presign_ent_url nomad ${NOMAD_VERSION_STRIPPED})
+    echo "NOMAD_VERSION_STRIPPED: ${NOMAD_VERSION_STRIPPED}"
+    echo "NOMAD_ENT_URL: ${NOMAD_ENT_URL}"
+    # export NOMAD_ENT_URL=$(AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_BINARY | base64 -d | gpg -d -) AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_BINARY} aws s3 presign --region="us-east-1" s3://${S3BUCKET}/vault-enterprise/${NOMAD_VERSION_STRIPPED}/vault-enterprise_${NOMAD_VERSION_STRIPPED}_linux_amd64.zip)
 
     # Replacing '+' with '-' as '+' is an invalid character for the AMI name and
     # the version isn't used during the install when 'NOMAD_ENT_URL' is populated
     export NOMAD_VERSION=${NOMAD_VERSION/'+'/'-'}
+    echo "NOMAD_VERSION: ${NOMAD_VERSION}"
   fi
 
   for TEMPLATE in $*; do
     echo "cd into ${TEMPLATE} directory"
     cd ${BUILDDIR}/$(echo ${TEMPLATE} | sed 's/-.*//')
-    echo "Building ${TEMPLATE}-aws.json Packer template..."
+    echo "Building ${TEMPLATE}.json Packer template..."
 
-    if /tmp/packer build ${TEMPLATE}-aws.json ; then
+    if /tmp/packer build ${TEMPLATE}.json ; then
       echo -e "\033[32m${TEMPLATE} \033[1m[PASS]\033[0m"
     else
       echo -e "\033[31m${TEMPLATE} \033[1m[FAIL]\033[0m"
@@ -107,13 +165,8 @@ build () {
   echo "Completed build from ${GIT_BRANCH}"
 }
 
-# TODO: Remove when merging into master
-build_ent () {
-  echo "build_ent is DEPRECATED: Remove this function when merging to master"
-}
-
 publish () {
-  # Exit early if not on master branch or RUN_BUILD env var not passed
+  # Exit early if not on master branch or RUN_PUBLISH env var not passed
   echo "GIT_BRANCH: ${GIT_BRANCH}"
   echo "RUN_PUBLISH: ${RUN_PUBLISH}"
 
