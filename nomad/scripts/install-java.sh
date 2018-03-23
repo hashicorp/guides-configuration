b31013c3a4c889d103e4b0b71e0c0ac66a2276e7
Jon Benson <bensojon@gmail.com>
2018-03-22 17:59:35 -0700

    Rename java script

--- nomad/scripts/install-java.sh
+++ nomad/scripts/install-java.sh
@@ -14,7 +14,7 @@ download_jdk() {
   readonly URL="http://www.oracle.com"
   readonly JDK_DOWNLOAD_URL1="${URL}/technetwork/java/javase/downloads/index.html"
   readonly JDK_DOWNLOAD_URL2=$(${curl_cmd} ${JDK_DOWNLOAD_URL1} | egrep -o "\/technetwork\/java/\javase\/downloads\/jdk${JDK_VERSION}-downloads-.+?\.html" | head -1 | cut -d '"' -f 1)
-  [[ -z "${JDK_DOWNLOAD_URL2}" ]] && logger "Could not get jdk download url - ${JDK_DOWNLOAD_URL1}" && exit 1
+  [[ -z "${JDK_DOWNLOAD_URL2}" ]] && echo "Could not get jdk download url - ${JDK_DOWNLOAD_URL1}" && exit 1
   readonly JDK_DOWNLOAD_URL3="${URL}${JDK_DOWNLOAD_URL2}"
   readonly JDK_DOWNLOAD_URL4=$(${curl_cmd} ${JDK_DOWNLOAD_URL3} | egrep -o "http\:\/\/download.oracle\.com\/otn-pub\/java\/jdk\/[7-8]u[0-9]+\-(.*)+\/jdk-[7-8]u[0-9]+(.*)linux-x64.${EXT}" | tail -1)
   for DL_URL in "${JDK_DOWNLOAD_URL4[@]}"; do
