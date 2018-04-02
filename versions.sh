4df608861f2c2ff04ba7d184b32571b788d82f0c
Jon Benson <bensojon@gmail.com>
2018-04-02 14:21:07 -0700

    Update versions and remove deregister to get past auth failure

--- versions.sh
+++ versions.sh
@@ -6,7 +6,7 @@
 export CONSUL_VERSION="1.0.6"
 
 # X.Y.Z or X.Y.Z+ent for Enterprise binary (e.g. 0.8.1 or 0.8.1+ent)
-export VAULT_VERSION="0.9.5"
+export VAULT_VERSION="0.9.6"
 
 # X.Y.Z (e.g. 0.6.2)
 export NOMAD_VERSION="0.7.1"
@@ -17,11 +17,11 @@ export PACKER_VERSION="1.1.3"
 # X.Y.Z (e.g. 0.10.0)
 export TERRAFORM_VERSION="0.11.1"
 
-# Production release: X.Y.Z (e.g. 0.1.1)
-# Release candidate: X.Y.Z-rcX (e.g. 0.1.1-rc1)
-# Beta release: X.Y.Z-betaX (e.g. 0.1.1-beta1)
-# Development branch: X.Y.Z-devX (e.g. 0.1.1-dev1)
-export RELEASE_VERSION="0.1.1-dev1"
+# Production release: X.Y.Z (e.g. 0.1.0)
+# Release candidate: X.Y.Z-rcX (e.g. 0.1.0-rc1)
+# Beta release: X.Y.Z-betaX (e.g. 0.1.0-beta1)
+# Development branch: X.Y.Z-devX (e.g. 0.1.0-dev1)
+export RELEASE_VERSION="0.1.0-dev1"
 
 # Force build or run on feature branch
 export RUN_BUILD=true
