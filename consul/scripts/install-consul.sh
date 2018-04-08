b56b74a051814752ae38de81acfe8be9a541ab31
Jon Benson <bensojon@gmail.com>
2018-04-07 19:33:46 -0700

    Fix CONSU?L_HTTP_ADDR

--- consul/scripts/install-consul.sh
+++ consul/scripts/install-consul.sh
@@ -12,7 +12,6 @@ CONSUL_CONFIG_DIR=/etc/consul.d
 CONSUL_DATA_DIR=/opt/consul/data
 CONSUL_TLS_DIR=/opt/consul/tls
 CONSUL_ENV_VARS=${CONSUL_CONFIG_DIR}/consul.conf
-CONSUL_PROFILE_SCRIPT=/etc/profile.d/consul.sh
 
 echo "Downloading Consul ${CONSUL_VERSION}"
 [ 200 -ne $(curl --write-out %{http_code} --silent --output /tmp/${CONSUL_ZIP} ${CONSUL_URL}) ] && exit 1
@@ -29,16 +28,12 @@ sudo mkdir -pm 0755 ${CONSUL_CONFIG_DIR} ${CONSUL_DATA_DIR} ${CONSUL_TLS_DIR}
 echo "Start Consul in -dev mode"
 cat <<ENVVARS | sudo tee ${CONSUL_ENV_VARS}
 FLAGS=-dev -ui
+CONSUL_HTTP_ADDR=http://127.0.0.1:8500
 ENVVARS
 
 echo "Update directory permissions"
 sudo chown -R ${USER}:${GROUP} ${CONSUL_CONFIG_DIR} ${CONSUL_DATA_DIR} ${CONSUL_TLS_DIR}
 sudo chmod -R 0644 ${CONSUL_CONFIG_DIR}/*
-
-echo "Set Consul profile script"
-cat <<PROFILE | sudo tee ${CONSUL_PROFILE_SCRIPT}
-export CONSUL_HTTP_ADDR=http://127.0.0.1:8500
-PROFILE
 
 echo "Give consul user shell access for remote exec"
 sudo /usr/sbin/usermod --shell /bin/bash ${USER} >/dev/null
