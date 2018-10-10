44d07cc84f8c0e3080fd702c0a9f9d00c110e6fe
Jon Benson <bensojon@gmail.com>
2018-10-10 12:29:20 -0700

    Push new ent images

--- versions.sh
+++ versions.sh
@@ -13,16 +13,16 @@ export TERRAFORM_VERSION="0.11.1"
 # Release candidate: X.Y.Z-rcX (e.g. 0.1.0-rc1)
 # Beta release: X.Y.Z-betaX (e.g. 0.1.0-beta1)
 # Development branch: X.Y.Z-f-branch (e.g. 0.1.0-f-branch)
-export RELEASE_VERSION="0.1.0"
+export RELEASE_VERSION="0.1.1"
 
 # X.Y.Z or X.Y.Z-ent for Enterprise binary (e.g. 1.0.6 or 1.0.6-ent)
-export CONSUL_VERSION="1.2.3"
+export CONSUL_VERSION="1.2.3-ent"
 
 # X.Y.Z or X.Y.Z-ent for Enterprise binary (e.g. 0.10.0 or 0.10.0-ent)
-export VAULT_VERSION="0.11.3"
+export VAULT_VERSION="0.11.3-ent"
 
 # X.Y.Z or X.Y.Z-ent for Enterprise binary (e.g. 0.8.0 or 0.8.0-ent)
-export NOMAD_VERSION="0.8.6"
+export NOMAD_VERSION="0.8.6-ent"
 
 # The below are aggregate lists of product versions to be published. Any
 # time a product version above is updated, that new version _must_ be
@@ -38,10 +38,10 @@ export NOMAD_VERSION="0.8.6"
 # To make a `release_versions` images public, set the version map value to
 # `true` or `false`. Enterprise images will _not_ be set to public even if
 # `true` is set.
-export RELEASE_VERSIONS='release_versions=[{"0.1.0"=true},]'
-export CONSUL_VERSIONS='consul_versions=["1.2.3",]'
-export VAULT_VERSIONS='vault_versions=["0.11.3",]'
-export NOMAD_VERSIONS='nomad_versions=["0.8.6",]'
+export RELEASE_VERSIONS='release_versions=[{"0.1.0"=true},{"0.1.1"=false},]'
+export CONSUL_VERSIONS='consul_versions=["1.2.3","1.2.3-ent",]'
+export VAULT_VERSIONS='vault_versions=["0.11.3","0.11.3-ent",]'
+export NOMAD_VERSIONS='nomad_versions=["0.8.6","0.8.6-ent",]'
 
 # Force build or run on feature branch
 export RUN_BUILD=true
